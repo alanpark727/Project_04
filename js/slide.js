4f2642e4d930d0eb4bda4074de5774ca5b0c7591
alanpark727 <drasel1234@naver.com>
2024-03-21 03:24:35 +0900

    2024.03.21 project4
    메인 슬라이드 1단계 구현 / 무한 슬라이드 구현하기 / 세부사항 다듬기

---
+++ js/slide.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+  let currentIndex = 0;
+  let slides = $(".slideItem");
+
+  function showSlide(index) {
+    slides.css('flex-grow','0').css('flex-basis','0px');
+
+    slides.eq(index).css('flex-grow','1').css('flex-basis','0px');
+
+    slides.eq(index).children().addClass("active");
+
+    slides.eq(index).children().siblings().removeClass("active");
+
+    slides.eq(index+1).css('flex-grow','0').css('flex-basis','46px');
+
+    slides.eq(index+2).css('flex-grow','0').css('flex-basis','46px');
+
+    currentIndex = index;
+
+  }
+
+  function nextSlide() {
+    let nextIndex = (currentIndex + 1) % slides.length;
+    showSlide(nextIndex);
+  }
+
+  function prevSlide() {
+    let prevIndex = (currentIndex - 1) % slides.length;
+    showSlide(prevIndex);
+  }
+
+  setInterval(nextSlide, 5000);
+
+  $("#next_1").on("click", function(){
+    nextSlide();
+  });
+  $("#prev_1").on("click", function(){
+    prevSlide();
+  });
+
+});
