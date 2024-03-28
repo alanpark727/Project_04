ae7f397a6d3b5e8c2f359438cdb4eb4d5b5772c4
alanpark727 <drasel1234@naver.com>
2024-03-28 17:42:38 +0900

    2024.03.28 project4
    서브페이지 스크립트 다듬기 / 메인과 연결하기 / 문서작업 구상하기 / 메인 슬라이더 수정하기

--- js/sub.js
+++ js/sub.js
@@ -36,6 +36,38 @@ $(document).ready(function(){
     });
 
 
+    // 작품 소개 펼치기
+
+    $('.intro_para').click(function(){
+        $('.intro_para').toggleClass('folded');
+    });
+    $('.intro_btn').click(function(){
+        $('.intro_para').removeClass('folded');
+        $('.intro_btn').hide();
+        $('.intro_fold').show();
+    });
+    $('.intro_fold').click(function(){
+        $('.intro_para').addClass('folded');
+        $('.intro_btn').show();
+        $('.intro_fold').hide();
+    });
+
+    // 출판사 서평 펼치기
+
+    $('.write_text').click(function(){
+        $('.write_text').toggleClass('folded');
+    });
+    $('.pub_btn').click(function(){
+        $('.write_text').removeClass('folded');
+        $('.pub_btn').hide();
+        $('.pub_fold').show();
+    });
+    $('.pub_fold').click(function(){
+        $('.write_text').addClass('folded');
+        $('.pub_btn').show();
+        $('.pub_fold').hide();
+    });
+
     // 저자 프로필 전환
 
     $('.author_profile>.profile_nav li:first').click(function(){
@@ -68,4 +100,81 @@ $(document).ready(function(){
         $('.latest, .popular').hide();
     });
 
+
+    // 저자 소개 서평 펼치기
+
+    $('.author_text').click(function(){
+        $('.author_text').toggleClass('folded');
+    });
+    $('.aut_btn').click(function(){
+        $('.author_text').removeClass('folded');
+        $('.aut_btn').hide();
+        $('.aut_fold').show();
+    });
+    $('.aut_fold').click(function(){
+        $('.author_text').addClass('folded');
+        $('.aut_btn').show();
+        $('.aut_fold').hide();
+    });
+
+
+    // 리뷰 별 파트
+
+    $('.star_area>label').click(function(){
+        alert('로그인 상태에서 가능합니다. 로그인 페이지로 이동합니다.');
+    })
+    $('.write_btn > button').click(function(){
+        $('.write_review_alert').slideDown();
+    });
+
+    $('.write_alert_close').click(function(){
+        $('.write_review_alert').slideUp();
+    })
+
+    let i = $('.star_area>label').index();
+    $('.star_area>label').hover(function(){
+        let j = $(this).index();
+        $(this).eq(i).addClass('active');
+        $(this).eq(i).siblings().removeClass('active');
+        console.log(j)
+        $('.star_tip').eq(j/2).addClass('active');
+        $('.star_tip').eq(j/2).siblings().removeClass('active');
+
+    }, function(){
+        $(this).removeClass('active');
+        $('.star_tip').removeClass('active');
+        $('.tip_title').addClass('active');
+    })
+
+    // 리뷰 하단 파트
+
+    $('.tab_1>li:first').click(function(){
+        $('.tab_1 a:first').addClass('active');
+        $('.tab_1 a:last').removeClass('active');
+        $('.tab_2:first').addClass('active');
+        $('.tab_2:last').removeClass('active');
+    });
+    $('.tab_1>li:last').click(function(){
+        $('.tab_1 a:last').addClass('active');
+        $('.tab_1 a:first').removeClass('active');
+        $('.tab_2:last').addClass('active');
+        $('.tab_2:first').removeClass('active');
+    });
+
+    $('.tab_2 li').click(function(){
+        $(this).children('a').addClass('active');
+        $(this).siblings().children('a').removeClass('active');
+    });
+
+    $('.qna_btn').click(function(){
+        $(this).toggleClass('active');
+        $('.qna_contents').slideToggle();
+    })
+
+
+    $(".footUnderbar>button").click(function(){
+        $(".footUnderbar>button").toggleClass('arrow');
+        $('#foot_info').toggleClass('open');
+      });
+    
 });
