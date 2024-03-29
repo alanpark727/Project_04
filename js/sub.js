89e20e9d19fe411f421edb89eae1ce9cd0da7935
alanpark727 <drasel1234@naver.com>
2024-03-29 10:01:40 +0900

    2024.03.29 project4
    서브텍스트파일 경로 수정

--- js/sub.js
+++ js/sub.js
@@ -3,35 +3,35 @@ $(document).ready(function(){
 
     // 텍스트 Mount
 
-    $.get("../sub_txt/book_detail.txt", function (data) {
+    $.get("/sub_txt/book_detail.txt", function (data) {
         $(".book_info .book_detail").html(data);
     });
 
-    $.get("../sub_txt/intro_text.txt", function (data) {
+    $.get("/sub_txt/intro_text.txt", function (data) {
         $(".intro_content .intro_text").html(data);
     });
 
-    $.get("../sub_txt/write_text.txt", function (data) {
+    $.get("/sub_txt/write_text.txt", function (data) {
         $(".pub_write .write_text").html(data);
     });
 
-    $.get("../sub_txt/higashino.txt", function (data) {
+    $.get("/sub_txt/higashino.txt", function (data) {
         $("#higashino").html(data);
     });
 
-    $.get("../sub_txt/goeun.txt", function (data) {
+    $.get("/sub_txt/goeun.txt", function (data) {
         $("#goeun").html(data);
     });
 
-    $.get("../sub_txt/author_text.txt", function (data) {
+    $.get("/sub_txt/author_text.txt", function (data) {
         $(".author_intro .author_text").html(data);
     });
 
-    $.get("../sub_txt/chapter_text.txt", function (data) {
+    $.get("/sub_txt/chapter_text.txt", function (data) {
         $(".book_chapter .chapter_text").html(data);
     });
 
-    $.get("../sub_txt/bot_qna.txt", function (data) {
+    $.get("/sub_txt/bot_qna.txt", function (data) {
         $(".bot_qna .qna_contents").html(data);
     });
 
