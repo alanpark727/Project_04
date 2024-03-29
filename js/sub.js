1e3e3d169cb39d9f9eeb72879d9fca88fdde5fac
alanpark727 <drasel1234@naver.com>
2024-03-29 10:10:55 +0900

    2024.03.29 project4
    파일 꼬인 것 일부 해결

--- js/sub.js
+++ js/sub.js
@@ -3,35 +3,35 @@ $(document).ready(function(){
 
     // 텍스트 Mount
 
-    $.get("/project_04/sub_txt/book_detail.txt", function (data) {
+    $.get("../sub_txt/book_detail.txt", function (data) {
         $(".book_info .book_detail").html(data);
     });
 
-    $.get("/sub_txt/intro_text.txt", function (data) {
+    $.get("../sub_txt/intro_text.txt", function (data) {
         $(".intro_content .intro_text").html(data);
     });
 
-    $.get("/sub_txt/write_text.txt", function (data) {
+    $.get("../sub_txt/write_text.txt", function (data) {
         $(".pub_write .write_text").html(data);
     });
 
-    $.get("/sub_txt/higashino.txt", function (data) {
+    $.get("../sub_txt/higashino.txt", function (data) {
         $("#higashino").html(data);
     });
 
-    $.get("/sub_txt/goeun.txt", function (data) {
+    $.get("../sub_txt/goeun.txt", function (data) {
         $("#goeun").html(data);
     });
 
-    $.get("/sub_txt/author_text.txt", function (data) {
+    $.get("../sub_txt/author_text.txt", function (data) {
         $(".author_intro .author_text").html(data);
     });
 
-    $.get("/sub_txt/chapter_text.txt", function (data) {
+    $.get("../sub_txt/chapter_text.txt", function (data) {
         $(".book_chapter .chapter_text").html(data);
     });
 
-    $.get("/sub_txt/bot_qna.txt", function (data) {
+    $.get("../sub_txt/bot_qna.txt", function (data) {
         $(".bot_qna .qna_contents").html(data);
     });
 
@@ -172,6 +172,16 @@ $(document).ready(function(){
     })
 
 
+    // 책 팝업
+
+    $('.book_cover').click(function(){
+        $('#popup_cover').fadeIn('fast');
+    });
+    $('.popup_box>.close_btn').click(function(){
+        $('#popup_cover').fadeOut('fast');
+    });
+
+    // 푸터
     $(".footUnderbar>button").click(function(){
         $(".footUnderbar>button").toggleClass('arrow');
         $('#foot_info').toggleClass('open');
